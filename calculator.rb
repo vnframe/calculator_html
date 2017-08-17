73ef356e0edef8e29125d3c2a6e7a5582a452614
vnframe <vnframe95@icloud.com>
2017-08-17 14:00:31 -0400

    comments

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,12 @@
 def calculate(cal, num1, num2)
   if cal == "add"
-    sum = num1.to_i + num2.to_i
+    sum = num1.to_i + num2.to_i #add numbers that are passed in via an argument
   elsif cal == "subt"
-    sum = num1.to_i - num2.to_i
+    sum = num1.to_i - num2.to_i #subtracts numbers that are passed in via an argument
   elsif cal == "div"
-    sum = num1.to_f/num2.to_f
+    sum = num1.to_f/num2.to_f #divides numbers that are passed in via argument and changes them to floats to account for decimals and zero
   elsif cal == "mult"
-    sum = num1.to_i * num.to_i
+    sum = num1.to_i * num.to_i #multiplies numbers that are passed via an argument
   end
-  sum.to_s
+  sum.to_s #changes the sum to a string
 end
